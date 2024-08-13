befbb13891e25e75f93c917b8763ca4526cfb846
Ubiratan Tavares <ubiratanntavares@gmail.com>
2024-08-13 20:40:04 -0300

    Rotas

--- src/app/temp/page.jsx
+++ src/app/temp/page.jsx
@@ -6,10 +6,10 @@ export default function Temp(){
         <div className="containerGlobal"> 
             <h1>Telas do sistema</h1>
 
-            <Link href="/" className="{styles.linkTemp">Home</Link>
-            <Link href="/sobre" className="{styles.linkTemp">Cadastro</Link>
-            <Link href="/usuarios/cadastro" className="{styles.linkTemp">Cadastro</Link>
-            <Link href="/usuarios/login" className="{styles.linkTemp">Login</Link>
+            <Link href="/" className={styles.linkTemp}>Home</Link>
+            <Link href="/sobre" className={styles.linkTemp}>Cadastro</Link>
+            <Link href="/usuarios/cadastro" className={styles.linkTemp}>Cadastro</Link>
+            <Link href="/usuarios/login" className={styles.linkTemp}>Login</Link>
         </div>
     )
 }
